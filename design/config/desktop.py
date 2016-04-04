ad8752de0fbc297b14409db164cabd9c7a3e34b1
bobzz-zone <bobzz.zone@gmail.com>
2016-04-04 12:24:09 -0400

    pertamax

---
+++ design/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Design",
+			"color": "blue",
+			"icon": "octicon octicon-list-unordered",
+			"type": "module",
+			"label": _("Design")
+		}
+	]
---
+++ design/hooks.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+app_name = "design"
+app_title = "Design"
+app_publisher = "bobzz.zone@gmail.com"
+app_description = "created by bobzz.zone@gmail.com"
+app_icon = "octicon octicon-list-unordered"
+app_color = "blue"
+app_email = "bobzz.zone@gmail.com"
+app_version = "0.0.1"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/design/css/design.css"
+# app_include_js = "/assets/design/js/design.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/design/css/design.css"
+# web_include_js = "/assets/design/js/design.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Website user home page (by function)
+# get_website_user_home_page = "design.utils.get_home_page"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "design.install.before_install"
+# after_install = "design.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "design.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"design.tasks.all"
+# 	],
+# 	"daily": [
+# 		"design.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"design.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"design.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"design.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "design.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "design.event.get_events"
+# }
+

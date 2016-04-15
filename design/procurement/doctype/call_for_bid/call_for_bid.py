a4ca60286069f7bdf14e6ae894b844047e04d785
bobzz-zone <bobzz.zone@gmail.com>
2016-04-15 22:54:34 +0700

    make button

--- design/procurement/doctype/call_for_bid/call_for_bid.py
+++ design/procurement/doctype/call_for_bid/call_for_bid.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2015, bobzz.zone@gmail.com and contributors
 # For license information, please see license.txt
-
 from __future__ import unicode_literals
 import frappe
+from frappe import db,throw,msgprint
+from frappe.utils import flt,cint
 from frappe.model.document import Document
+
 
 class CallForBid(Document):
 	pass
+	def make_quotation(self):
+		if len(self.product)==0:
+			frappe.throw("Please enter the product")
+		if len(self.supplier)==0:
+			frappe.throw("Please specify the supplier")
+		for s in self.supplier:
+			product=[]
+			for p in self.product:
+				p.append({
+					"doctype":"Supplier Quotation Item",
+					"item_code":p.item,
+					"qty":p.qty
+				})
+			quote = {
+					"doctype":"Supplier Quotation",
+					"transaction_date":self.date
+				}
+			quote.append(product)
+			row=frappe.get_doc(quote)
+			row.insert(ignore_permissions=True)

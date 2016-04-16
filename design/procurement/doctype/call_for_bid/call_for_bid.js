a77fa676489743d07266df0807c39e3c82eb313b
bobzz-zone <bobzz.zone@gmail.com>
2016-04-17 00:13:59 +0700

    cfb

--- design/procurement/doctype/call_for_bid/call_for_bid.js
+++ design/procurement/doctype/call_for_bid/call_for_bid.js
@@ -9,3 +9,34 @@ frappe.ui.form.on('Call For Bid', {
 cur_frm.add_fetch("item","item_name","item_name");
 cur_frm.add_fetch("item","stock_uom","uom");
 cur_frm.add_fetch("item","default_warehouse","warehouse");
+cur_frm.cscript.view = function(doc){
+	frappe.route_options = {"Supplier Quotation.call_for_bid": self.name};
+	frappe.set_route("List", "Supplier Quotation");
+}
+
+cur_frm.set_query("item_code", "items", function() {		
+	return{
+		query: "erpnext.controllers.queries.item_query",
+		filters: { 'is_purchase_item': 1 }
+	}
+});
+
+cur_frm.cscript.best = function(doc){
+		var best="";
+		var value=0;
+		var data = doc.supplier;
+		for (var i=0;i<data.length;i++){
+			if (data.received==1){
+				if value==0:
+					value=data.price
+					best=data.quotation
+				}else if (value>data.price){
+					value=data.price
+					best=data.quotation
+				}
+		if (best==""){
+			frappe.throw("Data is Incomplete")
+		}else{
+			frappe.set_route("Form", "Supplier Quotation",best)
+		}
+}

cdd4ad5e0bf32878003803d5f0b0bbd73e8e738c
bobzz-zone <bobzz.zone@gmail.com>
2016-04-18 20:51:41 +0700

    procuremnet menu

---
+++ design/config/procurement.py
@@ -0,0 +1,25 @@
+from frappe import _
+
+def get_data():
+	return [
+		"label":"Documents",
+		"items":[
+			{
+				"type":"doctype",
+				"name":"Call For Bid"
+			},{
+				"type":"doctype",
+				"name":"RFQ subcontractor"
+			},{
+				"type":"doctype",
+				"name":"RFQ supplier"
+			},{
+				"type":"doctype",
+				"name":"Supplier evaluation form"
+			},{
+				"type":"doctype",
+				"name":"Purchase Order"
+			}
+
+		]
+	]
